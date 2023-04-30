ac2af0b7e890bef7776bd9b9e0317c2187954113
Shankar Raj Dhanashetti <shankar.dhanashetti@capgemini.com>
2023-04-30 19:30:10 +0530

    Reverse vowels - improved version

--- src/basic/programs/ReverseVowelsInString.java
+++ src/basic/programs/ReverseVowelsInString.java
@@ -14,13 +14,13 @@ public class ReverseVowelsInString {
 		StringBuilder sb = new StringBuilder();
 		char[] arr = s.toCharArray();
 		for(int i=arr.length-1; i>=0; i--) {
-			if(arr[i] == 'a' ||arr[i] == 'e' ||arr[i] == 'i' ||arr[i] == 'o' ||arr[i] == 'u')
+			if(isVowel(arr[i]))
 				sb.append(arr[i]);
 		}
 		
 		int j =0;
 		for(int i=0; i<arr.length; i++) {
-			if(arr[i] == 'a' ||arr[i] == 'e' ||arr[i] == 'i' ||arr[i] == 'o' ||arr[i] == 'u')
+			if(isVowel(arr[i]))
 			{
 				arr[i] = sb.charAt(j);
 				j++;
@@ -33,7 +33,49 @@ public class ReverseVowelsInString {
 	
 	public static void main(String[] args) {
 		String str = "ssdhanashetti";
-		System.out.println(reverseVowels1(str));
+		System.out.println(reverseVowel(str));
+		System.out.println(reverseVowel(str).equals(reverseVowels1(str)));
 	}
+	
+	//Better approach
+	static String reverseVowel(String str) {
+		// Start two indexes from two corners
+		// and move toward each other
+		int i = 0;
+		int j = str.length()-1;
+		char[] str1 = str.toCharArray();
+		while (i < j)
+		{
+			if (!isVowel(str1[i]))
+			{
+				i++;
+				continue;
+			}
+			if (!isVowel(str1[j]))
+			{
+				j--;
+				continue;
+			}
+
+			// swapping
+			char t = str1[i];
+			str1[i]= str1[j];
+			str1[j]= t;
+			
+
+			i++;
+			j--;
+		}
+		String str2 = String.copyValueOf(str1);
+		return str2;
+	}
+	
+	// utility function to check for vowel
+		static boolean isVowel(char c) {
+			return (c == 'a' || c == 'A' || c == 'e'
+					|| c == 'E' || c == 'i' || c == 'I'
+					|| c == 'o' || c == 'O' || c == 'u'
+					|| c == 'U');
+		}
 
 }
